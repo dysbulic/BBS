6d9683a866f67539ca06c18facf6f8c7d88de7be
Will Holcomb <will@dhappy.org>
2010-12-09 04:44:59 -0800

    fixed missing include

--- create_test_data.php
+++ create_test_data.php
@@ -1,9 +1,12 @@
 <?php
 
-for($i = 1; $i <= 3; $i++) {
-  $post = new Post('Test #' . i,
-		   'Test content',
-		   'test');
+require_once( 'User.php' );
+require_once( 'Post.php' );
+
+for( $i = 1; $i <= 3; $i++ ) {
+  $post = new Post( 'Test #' . i,
+		    'Test content',
+		    'test' );
   $post->save();
 }
 
